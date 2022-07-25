af83ab90d83e188c797abd9e40fd935832c5418b
ZairullahDeveloper <zairullahfreefire@outlook.com>
2022-07-25 21:01:45 +0700

    Denpendencies

---
+++ termuxsetup.sh
@@ -0,0 +1,14 @@
+apt update && apt upgrade
+
+apt install clang python libjpeg-turbo libpng
+export SODIUM_INSTALL=system 
+
+arch=$(uname -m)
+
+if [[ $arch == aarch64 ]]; then
+    echo "64 bit detected"
+    export LDFLAGS="-L/system/lib64"
+    pip install -r requirements.txt
+else
+    pip install -r requirements.txt
+fi
