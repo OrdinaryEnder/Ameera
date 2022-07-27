25b1056dcbce488544633e14895133ee4da7da32
ZairullahDeveloper <zairullahfreefire@outlook.com>
2022-07-27 17:49:43 +0700

    Update

--- termuxsetup.sh
+++ termuxsetup.sh
@@ -1,6 +1,6 @@
 apt update && apt upgrade
 
-apt install clang python libjpeg-turbo libpng
+apt install clang python libjpeg-turbo libpng libsodium libffi openssl
 export SODIUM_INSTALL=system 
 
 arch=$(uname -m)
@@ -9,6 +9,10 @@ if [[ $arch == aarch64 ]]; then
     echo "64 bit detected"
     export LDFLAGS="-L/system/lib64"
     pip install -r requirements.txt
+elif [[ $arch == arm64]]; then
+   echo "64 Bit Detected"
+   export LDFLAGS="-L/system/lib64"
+   pip install -r requirements.txt
 else
     pip install -r requirements.txt
 fi
