bf3fe489be39c5d28e7007341be49ddbdfb8ee60
ZairullahDeveloper <zairullahfreefire@outlook.com>
2022-07-27 17:51:06 +0700

    Update

--- termuxsetup.sh
+++ termuxsetup.sh
@@ -9,7 +9,7 @@ if [[ $arch == aarch64 ]]; then
     echo "64 bit detected"
     export LDFLAGS="-L/system/lib64"
     pip install -r requirements.txt
-elif [[ $arch == arm64]]; then
+elif [[ $arch == arm64 ]]; then
    echo "64 Bit Detected"
    export LDFLAGS="-L/system/lib64"
    pip install -r requirements.txt
