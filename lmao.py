2ef05d188a3b10b432d5184afffef6024cb5b0ca
OrdinaryEnder <zairullahfreefire@outlook.com>
2022-10-06 04:15:11 +0700

    4 am

--- lmao.py
+++ lmao.py
@@ -104,8 +104,8 @@ async def on_wavelink_node_ready(node: wavelink.Node):
     print(Fore.GREEN + f"Node {node.identifier} is ready!")
 
 @bot.event
-async def on_wavelink_track_end(player: wavelink.Player, track: wavelink.Track, reason):
-    ctx = player.ctx
+async def on_wavelink_track_end(interaction: discord.Interaction, player: wavelink.Player, track: wavelink.Track, reason):
+    ctx = await self.bot.get_context(interaction, cls=commands.Context)
     vc: player = ctx.voice_client
 
     if vc.loop:
