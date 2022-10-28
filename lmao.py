336295f92e586e04d0bb8ff65713dc4976319755
OrdinaryEnder <zairullahfreefire@outlook.com>
2022-10-29 06:08:11 +0700

    bruh

--- lmao.py
+++ lmao.py
@@ -188,11 +188,12 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    if any(badword in message.content.lower() for badword in badwords):
-        authorava = await message.author.avatar.read()
-        await message.delete()
-        lmao = await message.channel.create_webhook(name=message.author.name, avatar=authorava)
-        await lmao.send("#" * len(message.content))
+    for badword in badwords:
+        if re.fullmatch(badword, message.content.lower()):
+            authorava = await message.author.avatar.read()
+            await message.delete()
+            lmao = await message.channel.create_webhook(name=message.author.name, avatar=authorava)
+            await lmao.send("#" * len(message.content))
 
 
 @bot.event
