0c28f51e20c7fa1d85234a3c60e2007a8a6856db
OrdinaryEnder <zairullahfreefire@outlook.com>
2022-12-25 08:12:08 +0700

    duckduckgoooo

--- heart/other.py
+++ heart/other.py
@@ -248,7 +248,7 @@ class Other(commands.Cog):
         embed = discord.Embed(title=f"**Search Matching for {searchbar}**")
         embed.set_author(name="DuckDuckGo", icon_url="https://upload.wikimedia.org/wikipedia/en/9/90/The_DuckDuckGo_Duck.png")
         for results in result[:10]:
-            embed.add_field(name=f"[{results.title}]({results.url})", value=results.description)
+            embed.add_field(name="​", value=f"[{results.title}]({results.url}) \n \n{results.description}")
 
         await interaction.followup.send(embed=embed)
 
