75c9c850b939dd12fb7c3cd1c0605271bbaa0ea7
OrdinaryEnder <zairullahfreefire@outlook.com>
2023-01-12 21:57:02 +0700

    amogus

--- heart/nsfw.py
+++ heart/nsfw.py
@@ -17,17 +17,15 @@ class nsfw(commands.Cog):
 
     @app_commands.command(name="image", description="Get Images (NSFW!!!!!)", nsfw=True)
     @app_commands.describe(image="NSFW Image about to show")
-    async def _image(self, interaction: discord.Interaction, image: typing.Literal['hass', 'hmidriff', 'pgif', 'hentai', 'holo', 'hneko', 'neko', 'hkitsune', 'kemonomimi', 'anal', 'hanal', 'gonewild', 'kanna', 'ass', 'pussy', 'thigh', 'hthigh', 'gah', 'coffee', 'food', 'paizuri', 'tentacle', 'boobs', 'hboobs', 'yaoi']):
+    async def _image(self, interaction: discord.Interaction, image: typing.Literal['4k', 'hentai', 'holo', 'neko', 'ass', 'boobs', 'hentai', 'lesbian', 'gasm',  'lewd', 'pussy', 'cum', 'blowjob', 'feet', 'spank', 'anal']):
         try:
             await interaction.response.defer()
             async with aiohttp.ClientSession() as session:
-                async with session.get(f"https://nekobot.xyz/api/image?type={image}") as r:
+                async with session.get(f"http://api.nekos.fun:8080/api/{image}") as r:
                     res = await r.json()
                     em = discord.Embed(title="Result")
                     em.set_image(url=res['message'])
                     await interaction.followup.send(embed=em)
-        except:
-            await interaction.followup.send(traceback.print_exc())
 
 
 async def setup(bot):
