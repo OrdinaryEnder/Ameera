6fd0dc60c248ee720cf9e47087667cdffc79a961
OrdinaryEnder <zairullahfreefire@outlook.com>
2023-05-04 15:00:10 +0700

    bad commit smu

--- heart/music.py
+++ heart/music.py
@@ -60,14 +60,14 @@ colorama.init(autoreset=True)
 class MusicDropDown(discord.ui.Select):
     def __init__(self, track, vc, message):
         ret = []
-        self.lel = {} # because long ass sc links
+        self.lel = [] # because long ass sc links
         self.vc = vc
         self.message = message
         self.executed = False
         for index, song in enumerate(track[:10]):
             ret.append(discord.SelectOption(label=song.title,
                        description=song.author, value=str(index)))
-            self.lel[song.title] = song.uri
+            lel.append(song.url)
 
         super().__init__(placeholder='Choose song ...',
                          min_values=1, max_values=1, options=ret)
