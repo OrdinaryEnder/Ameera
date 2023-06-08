95a7f29beb92c15697070791b76db77347d98f20
OrdinaryEnder <zairullahfreefire@outlook.com>
2023-06-08 13:12:47 +0700

    fix

--- heart/music.py
+++ heart/music.py
@@ -764,9 +764,8 @@ class Music(commands.Cog):
       thechannel = await thecategory.create_text_channel(f"{self.bot.user.name}-setup-music")
       await thecategory.create_voice_channel(f"{self.bot.user.name} Voice Channel")
       
-      if interaction.guild.voice_client:
-       vc: wavelink.Player = interaction.guild.voice_client
-      if vc and vc.is_playing():
+      if interaction.guild.voice_client and vc.is_playing():
+         vc: wavelink.Player = interaction.guild.voice_client
          embed = discord.Embed(title="**NOW PLAYING**", description=f"[{vc.track.title}]({vc.track.uri})")
          embed.set_image(url=(vc.track.thumbnail if hasattr(vc.track, "thumbnail") else "https://media.discordapp.net/attachments/977216545921073192/1033304783156690984/images2.jpg"))
       else:
