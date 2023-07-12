04cbca9cdf42b4973b12985c0916059d18b49609
OrdinaryEnder <zairullahfreefire@outlook.com>
2023-07-12 11:50:53 +0700

    sus

--- heart/music.py
+++ heart/music.py
@@ -784,10 +784,7 @@ class Music(commands.Cog):
       else:
           embed = discord.Embed(title="**Nothing currently playing right now ^^", description="Put some song to listen")
           embed.set_image(url="https://media.discordapp.net/attachments/977216545921073192/1116244099721343046/peakpx.jpg")
-6
 
-
-       
       waitermessage = await thechannel.send(embed=embed)
       view = MusicViewSetup()
       view.message = waitermessage
